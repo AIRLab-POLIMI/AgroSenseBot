e5e254db11670bcce10251b2017b9a607ef6b53d
Enri2077 <enry2077@gmail.com>
2023-09-26 13:35:20 +0200

    reformat

--- agrosensebot_dummy_canopen_nodes/include/ros2_bridge_node.h
+++ agrosensebot_dummy_canopen_nodes/include/ros2_bridge_node.h
@@ -19,93 +19,107 @@ using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 class VCUCANOpenSlaveNode;
+
 class MotorDriveCANOpenSlaveNode;
 
 class ROS2BridgeNode : public rclcpp_lifecycle::LifecycleNode {
-    std::string VCU_canopen_node_config_;
-    std::string MDL_canopen_node_config_;
-    std::string MDR_canopen_node_config_;
-    std::string FAN_canopen_node_config_;
-    std::string can_interface_name_;
-    std::chrono::milliseconds gcu_is_alive_timeout_ = 100ms;
+  std::string VCU_canopen_node_config_;
+  std::string MDL_canopen_node_config_;
+  std::string MDR_canopen_node_config_;
+  std::string FAN_canopen_node_config_;
+  std::string can_interface_name_;
+  std::chrono::milliseconds gcu_is_alive_timeout_ = 100ms;
 
-    std::thread VCU_canopen_node_thread_;
-    std::thread MDL_canopen_node_thread_;
-    std::thread MDR_canopen_node_thread_;
-    std::thread FAN_canopen_node_thread_;
-    std::shared_ptr <VCUCANOpenSlaveNode> VCU_canopen_slave_node_ = nullptr;
-    std::shared_ptr <MotorDriveCANOpenSlaveNode> MDL_canopen_slave_node_ = nullptr;
-    std::shared_ptr <MotorDriveCANOpenSlaveNode> MDR_canopen_slave_node_ = nullptr;
-    std::shared_ptr <MotorDriveCANOpenSlaveNode> FAN_canopen_slave_node_ = nullptr;
+  std::thread VCU_canopen_node_thread_;
+  std::thread MDL_canopen_node_thread_;
+  std::thread MDR_canopen_node_thread_;
+  std::thread FAN_canopen_node_thread_;
+  std::shared_ptr<VCUCANOpenSlaveNode> VCU_canopen_slave_node_ = nullptr;
+  std::shared_ptr<MotorDriveCANOpenSlaveNode> MDL_canopen_slave_node_ = nullptr;
+  std::shared_ptr<MotorDriveCANOpenSlaveNode> MDR_canopen_slave_node_ = nullptr;
+  std::shared_ptr<MotorDriveCANOpenSlaveNode> FAN_canopen_slave_node_ = nullptr;
 
-    std::atomic<bool> lifecycle_node_active_ = false;
-    rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::UInt8>::SharedPtr gcu_alive_pub_;
-    rclcpp_lifecycle::LifecyclePublisher<agrosensebot_canopen_bridge_msgs::msg::SpeedRef>::SharedPtr speed_ref_pub_;
-    rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>::SharedPtr motor_drive_left_sub_;
-    rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>::SharedPtr motor_drive_right_sub_;
-    rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>::SharedPtr motor_drive_fan_sub_;
-    rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::VCUState>::SharedPtr VCU_state_sub_;
-    rclcpp::TimerBase::SharedPtr gcu_is_alive_timer_;
+  std::atomic<bool> lifecycle_node_active_ = false;
+  rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::UInt8>::SharedPtr gcu_alive_pub_;
+  rclcpp_lifecycle::LifecyclePublisher<agrosensebot_canopen_bridge_msgs::msg::SpeedRef>::SharedPtr speed_ref_pub_;
+  rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>::SharedPtr motor_drive_left_sub_;
+  rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>::SharedPtr motor_drive_right_sub_;
+  rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>::SharedPtr motor_drive_fan_sub_;
+  rclcpp::Subscription<agrosensebot_canopen_bridge_msgs::msg::VCUState>::SharedPtr VCU_state_sub_;
+  rclcpp::TimerBase::SharedPtr gcu_is_alive_timer_;
 
-    bool last_VCU_alive_bit_ = false;
+  bool last_VCU_alive_bit_ = false;
 
-    rclcpp::Time last_GCU_message_time_ = rclcpp::Time(0);
-    rclcpp::Time last_GCU_alive_bit_change_time_ = rclcpp::Time(0);
-    bool last_GCU_alive_bit_ = false;
+  rclcpp::Time last_GCU_message_time_ = rclcpp::Time(0);
+  rclcpp::Time last_GCU_alive_bit_change_time_ = rclcpp::Time(0);
+  bool last_GCU_alive_bit_ = false;
 
-    void vcu_alive_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::VCUState::SharedPtr) ;
+  void vcu_alive_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::VCUState::SharedPtr);
 
-    void gcu_is_alive_timer_ros2_callback();
+  void gcu_is_alive_timer_ros2_callback();
 
-    void run_VCU_canopen_node();
-    void run_MDL_canopen_node();
-    void run_MDR_canopen_node();
-    void run_FAN_canopen_node();
+  void run_VCU_canopen_node();
+
+  void run_MDL_canopen_node();
+
+  void run_MDR_canopen_node();
+
+  void run_FAN_canopen_node();
 
 public:
-    explicit ROS2BridgeNode(const std::string &node_name, bool intra_process_comms = false)
-            : rclcpp_lifecycle::LifecycleNode(node_name, rclcpp::NodeOptions().use_intra_process_comms(
-                    intra_process_comms)) {
-        this->declare_parameter<std::string>("dummy_VCU_canopen_node_config", "test_slave.eds");
-        this->declare_parameter<std::string>("dummy_MDL_canopen_node_config", "test_slave.eds");
-        this->declare_parameter<std::string>("dummy_MDR_canopen_node_config", "test_slave.eds");
-        this->declare_parameter<std::string>("dummy_FAN_canopen_node_config", "test_slave.eds");
-        this->declare_parameter<std::string>("can_interface_name", "vcan0");
+  explicit ROS2BridgeNode(const std::string &node_name, bool intra_process_comms = false)
+          : rclcpp_lifecycle::LifecycleNode(node_name, rclcpp::NodeOptions().use_intra_process_comms(
+          intra_process_comms)) {
+    this->declare_parameter<std::string>("dummy_VCU_canopen_node_config", "test_slave.eds");
+    this->declare_parameter<std::string>("dummy_MDL_canopen_node_config", "test_slave.eds");
+    this->declare_parameter<std::string>("dummy_MDR_canopen_node_config", "test_slave.eds");
+    this->declare_parameter<std::string>("dummy_FAN_canopen_node_config", "test_slave.eds");
+    this->declare_parameter<std::string>("can_interface_name", "vcan0");
 
-        gcu_alive_pub_ = this->create_publisher<std_msgs::msg::UInt8>("test/gcu_alive", rclcpp::SensorDataQoS());
-        speed_ref_pub_ = this->create_publisher<agrosensebot_canopen_bridge_msgs::msg::SpeedRef>("test/speed_ref", rclcpp::SensorDataQoS());
+    gcu_alive_pub_ = this->create_publisher<std_msgs::msg::UInt8>(
+            "test/gcu_alive", rclcpp::SensorDataQoS());
+    speed_ref_pub_ = this->create_publisher<agrosensebot_canopen_bridge_msgs::msg::SpeedRef>(
+            "test/speed_ref", rclcpp::SensorDataQoS());
 
-        motor_drive_left_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>("test/motor_drive_left", rclcpp::SensorDataQoS(), std::bind(&ROS2BridgeNode::motor_drive_left_ros2_callback, this, _1));
-        motor_drive_right_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>("test/motor_drive_right", rclcpp::SensorDataQoS(), std::bind(&ROS2BridgeNode::motor_drive_right_ros2_callback, this, _1));
-        motor_drive_fan_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>("test/motor_drive_fan", rclcpp::SensorDataQoS(), std::bind(&ROS2BridgeNode::motor_drive_fan_ros2_callback, this, _1));
-        VCU_state_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::VCUState>("test/vcu_state", rclcpp::SensorDataQoS(), std::bind(&ROS2BridgeNode::vcu_alive_ros2_callback, this, _1));
+    motor_drive_left_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>(
+            "test/motor_drive_left", rclcpp::SensorDataQoS(),
+            std::bind(&ROS2BridgeNode::motor_drive_left_ros2_callback, this, _1));
+    motor_drive_right_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>(
+            "test/motor_drive_right", rclcpp::SensorDataQoS(),
+            std::bind(&ROS2BridgeNode::motor_drive_right_ros2_callback, this, _1));
+    motor_drive_fan_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::MotorDrive>(
+            "test/motor_drive_fan", rclcpp::SensorDataQoS(),
+            std::bind(&ROS2BridgeNode::motor_drive_fan_ros2_callback, this, _1));
+    VCU_state_sub_ = this->create_subscription<agrosensebot_canopen_bridge_msgs::msg::VCUState>(
+            "test/vcu_state", rclcpp::SensorDataQoS(),
+            std::bind(&ROS2BridgeNode::vcu_alive_ros2_callback, this, _1));
 
-    };
+  };
 
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-    on_configure(const rclcpp_lifecycle::State &) override;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State &) override;
 
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-    on_activate(const rclcpp_lifecycle::State &) override;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_activate(const rclcpp_lifecycle::State &) override;
 
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-    on_deactivate(const rclcpp_lifecycle::State &) override;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_deactivate(const rclcpp_lifecycle::State &) override;
 
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-    on_cleanup(const rclcpp_lifecycle::State &) override;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_cleanup(const rclcpp_lifecycle::State &) override;
 
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-    on_shutdown(const rclcpp_lifecycle::State &) override;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_shutdown(const rclcpp_lifecycle::State &) override;
 
-    void gcu_alive_canopen_callback(bool, bool, bool);
+  void gcu_alive_canopen_callback(bool, bool, bool);
 
-    void speed_ref_canopen_callback(int16_t, int16_t, int16_t);
+  void speed_ref_canopen_callback(int16_t, int16_t, int16_t);
 
-    void motor_drive_left_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr);
+  void motor_drive_left_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr);
 
-    void motor_drive_right_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr);
+  void motor_drive_right_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr);
 
-    void motor_drive_fan_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr);
+  void motor_drive_fan_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr);
 
 };
 
--- agrosensebot_dummy_canopen_nodes/src/ros2_bridge_node.cpp
+++ agrosensebot_dummy_canopen_nodes/src/ros2_bridge_node.cpp
@@ -4,62 +4,62 @@
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 ROS2BridgeNode::on_configure(const rclcpp_lifecycle::State &) {
-    lifecycle_node_active_.store(false);
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  lifecycle_node_active_.store(false);
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 ROS2BridgeNode::on_activate(const rclcpp_lifecycle::State &) {
-    get_parameter("dummy_VCU_canopen_node_config", VCU_canopen_node_config_);
-    get_parameter("dummy_MDL_canopen_node_config", MDL_canopen_node_config_);
-    get_parameter("dummy_MDR_canopen_node_config", MDR_canopen_node_config_);
-    get_parameter("dummy_FAN_canopen_node_config", FAN_canopen_node_config_);
-    get_parameter("can_interface_name", can_interface_name_);
-    RCLCPP_INFO(this->get_logger(), "dummy_canopen_node_config: %s", VCU_canopen_node_config_.c_str());
-    RCLCPP_INFO(this->get_logger(), "can_interface_name: %s", can_interface_name_.c_str());
+  get_parameter("dummy_VCU_canopen_node_config", VCU_canopen_node_config_);
+  get_parameter("dummy_MDL_canopen_node_config", MDL_canopen_node_config_);
+  get_parameter("dummy_MDR_canopen_node_config", MDR_canopen_node_config_);
+  get_parameter("dummy_FAN_canopen_node_config", FAN_canopen_node_config_);
+  get_parameter("can_interface_name", can_interface_name_);
+  RCLCPP_INFO(this->get_logger(), "dummy_canopen_node_config: %s", VCU_canopen_node_config_.c_str());
+  RCLCPP_INFO(this->get_logger(), "can_interface_name: %s", can_interface_name_.c_str());
 
-    if(can_interface_name_ != "vcan0"){
-        RCLCPP_FATAL(this->get_logger(), "Using a CAN interface different than vcan0 for testing!");
-        return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::FAILURE;
-    }
+  if (can_interface_name_ != "vcan0") {
+    RCLCPP_FATAL(this->get_logger(), "Using a CAN interface different than vcan0 for testing!");
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::FAILURE;
+  }
 
-    lifecycle_node_active_.store(true);
-    gcu_alive_pub_->on_activate();
-    speed_ref_pub_->on_activate();
+  lifecycle_node_active_.store(true);
+  gcu_alive_pub_->on_activate();
+  speed_ref_pub_->on_activate();
 
-    VCU_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_VCU_canopen_node, this));
-    MDL_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_MDL_canopen_node, this));
-    MDR_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_MDR_canopen_node, this));
-    FAN_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_FAN_canopen_node, this));
+  VCU_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_VCU_canopen_node, this));
+  MDL_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_MDL_canopen_node, this));
+  MDR_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_MDR_canopen_node, this));
+  FAN_canopen_node_thread_ = std::thread(std::bind(&ROS2BridgeNode::run_FAN_canopen_node, this));
 
-    std::chrono::duration gcu_is_alive_timer_period_ = 10ms;
-    gcu_is_alive_timer_ = rclcpp::create_timer(
-            this, this->get_clock(), rclcpp::Duration(gcu_is_alive_timer_period_),
-            std::bind(&ROS2BridgeNode::gcu_is_alive_timer_ros2_callback, this));
-    last_GCU_message_time_ = this->get_clock()->now();
-    last_GCU_alive_bit_change_time_ = this->get_clock()->now();
+  std::chrono::duration gcu_is_alive_timer_period_ = 10ms;
+  gcu_is_alive_timer_ = rclcpp::create_timer(
+          this, this->get_clock(), rclcpp::Duration(gcu_is_alive_timer_period_),
+          std::bind(&ROS2BridgeNode::gcu_is_alive_timer_ros2_callback, this));
+  last_GCU_message_time_ = this->get_clock()->now();
+  last_GCU_alive_bit_change_time_ = this->get_clock()->now();
 
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 ROS2BridgeNode::on_deactivate(const rclcpp_lifecycle::State &) {
-    lifecycle_node_active_.store(false);
-    VCU_canopen_node_thread_.join();
-    MDL_canopen_node_thread_.join();
-    MDR_canopen_node_thread_.join();
-    FAN_canopen_node_thread_.join();
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  lifecycle_node_active_.store(false);
+  VCU_canopen_node_thread_.join();
+  MDL_canopen_node_thread_.join();
+  MDR_canopen_node_thread_.join();
+  FAN_canopen_node_thread_.join();
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 ROS2BridgeNode::on_cleanup(const rclcpp_lifecycle::State &) {
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 ROS2BridgeNode::on_shutdown(const rclcpp_lifecycle::State &) {
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
 void ROS2BridgeNode::run_VCU_canopen_node() {
@@ -73,8 +73,8 @@ void ROS2BridgeNode::run_VCU_canopen_node() {
   io::CanChannel chan(poll, exec);
   chan.open(ctrl);
 
-  VCU_canopen_slave_node_ = std::make_shared<VCUCANOpenSlaveNode>(timer, chan,
-                                                                  VCU_canopen_node_config_, "", this);
+  VCU_canopen_slave_node_ = std::make_shared<VCUCANOpenSlaveNode>(
+          timer, chan, VCU_canopen_node_config_, "", this);
   VCU_canopen_slave_node_->Reset();
 
   while (lifecycle_node_active_.load()) {
@@ -94,8 +94,8 @@ void ROS2BridgeNode::run_MDL_canopen_node() {
   io::CanChannel chan(poll, exec);
   chan.open(ctrl);
 
-  MDL_canopen_slave_node_ = std::make_shared<MotorDriveCANOpenSlaveNode>(timer, chan,
-                                                                         MDL_canopen_node_config_, "", this);
+  MDL_canopen_slave_node_ = std::make_shared<MotorDriveCANOpenSlaveNode>(
+          timer, chan, MDL_canopen_node_config_, "", this);
   MDL_canopen_slave_node_->node_name_ = "dummy_MDL";
   MDL_canopen_slave_node_->Reset();
 
@@ -116,8 +116,8 @@ void ROS2BridgeNode::run_MDR_canopen_node() {
   io::CanChannel chan(poll, exec);
   chan.open(ctrl);
 
-  MDR_canopen_slave_node_ = std::make_shared<MotorDriveCANOpenSlaveNode>(timer, chan,
-                                                                         MDR_canopen_node_config_, "", this);
+  MDR_canopen_slave_node_ = std::make_shared<MotorDriveCANOpenSlaveNode>(
+          timer, chan, MDR_canopen_node_config_, "", this);
   MDR_canopen_slave_node_->node_name_ = "dummy_MDR";
   MDR_canopen_slave_node_->Reset();
 
@@ -138,8 +138,8 @@ void ROS2BridgeNode::run_FAN_canopen_node() {
   io::CanChannel chan(poll, exec);
   chan.open(ctrl);
 
-  FAN_canopen_slave_node_ = std::make_shared<MotorDriveCANOpenSlaveNode>(timer, chan,
-                                                                         FAN_canopen_node_config_, "", this);
+  FAN_canopen_slave_node_ = std::make_shared<MotorDriveCANOpenSlaveNode>(
+          timer, chan, FAN_canopen_node_config_, "", this);
   FAN_canopen_slave_node_->node_name_ = "dummy_FAN";
   FAN_canopen_slave_node_->Reset();
 
@@ -150,93 +150,94 @@ void ROS2BridgeNode::run_FAN_canopen_node() {
 }
 
 void ROS2BridgeNode::vcu_alive_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::VCUState::SharedPtr VCU_state_msg) {
-    last_VCU_alive_bit_ = not last_VCU_alive_bit_;
+  last_VCU_alive_bit_ = not last_VCU_alive_bit_;
 
-    bool pump_status_bit = false;  // TODO
-    uint8_t more_recent_alarm_id_to_confirm = 0;  // TODO
-    uint8_t more_recent_active_alarm_id = 0;  // TODO
+  bool pump_status_bit = false;  // TODO
+  uint8_t more_recent_alarm_id_to_confirm = 0;  // TODO
+  uint8_t more_recent_active_alarm_id = 0;  // TODO
 
-    if (VCU_canopen_slave_node_ != nullptr) {
-        VCU_canopen_slave_node_->send_TPDO_1(last_VCU_alive_bit_, VCU_state_msg->vcu_safety_status,
-                                             pump_status_bit,
-                                             VCU_state_msg->control_mode,
-                                             more_recent_alarm_id_to_confirm, more_recent_active_alarm_id);
-    }
+  if (VCU_canopen_slave_node_ != nullptr) {
+    VCU_canopen_slave_node_->send_TPDO_1(last_VCU_alive_bit_, VCU_state_msg->vcu_safety_status,
+                                         pump_status_bit,
+                                         VCU_state_msg->control_mode,
+                                         more_recent_alarm_id_to_confirm, more_recent_active_alarm_id);
+  }
 }
 
 void ROS2BridgeNode::motor_drive_left_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr msg) {
-    if (MDL_canopen_slave_node_ != nullptr) {
-        MDL_canopen_slave_node_->send_TPDO_1(
-                (int16_t)(msg->controller_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
-                (int16_t)(msg->motor_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
-                msg->motor_rpm,
-                (int16_t)(msg->battery_current_display * INVERSE_RAW_DATA_STEP_VALUE_current));
-        MDL_canopen_slave_node_->send_TPDO_2(0, 0, 0, 0);
-        MDL_canopen_slave_node_->send_TPDO_3(false); // TODO interlock_status
-        MDL_canopen_slave_node_->send_TPDO_4(0); // TODO rotor_position
-    }
+  if (MDL_canopen_slave_node_ != nullptr) {
+    MDL_canopen_slave_node_->send_TPDO_1(
+            (int16_t) (msg->controller_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
+            (int16_t) (msg->motor_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
+            msg->motor_rpm,
+            (int16_t) (msg->battery_current_display * INVERSE_RAW_DATA_STEP_VALUE_current));
+    MDL_canopen_slave_node_->send_TPDO_2(0, 0, 0, 0);
+    MDL_canopen_slave_node_->send_TPDO_3(false); // TODO interlock_status
+    MDL_canopen_slave_node_->send_TPDO_4(0); // TODO rotor_position
+  }
 
 }
 
 void ROS2BridgeNode::motor_drive_right_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr msg) {
-    if (MDR_canopen_slave_node_ != nullptr) {
-        MDR_canopen_slave_node_->send_TPDO_1(
-                (int16_t)(msg->controller_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
-                (int16_t)(msg->motor_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
-                msg->motor_rpm,
-                (int16_t)(msg->battery_current_display * INVERSE_RAW_DATA_STEP_VALUE_current));
-        MDR_canopen_slave_node_->send_TPDO_2(0, 0, 0, 0);
-        MDR_canopen_slave_node_->send_TPDO_3(false); // TODO interlock_status
-        MDR_canopen_slave_node_->send_TPDO_4(0); // TODO rotor_position
-    }
+  if (MDR_canopen_slave_node_ != nullptr) {
+    MDR_canopen_slave_node_->send_TPDO_1(
+            (int16_t) (msg->controller_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
+            (int16_t) (msg->motor_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
+            msg->motor_rpm,
+            (int16_t) (msg->battery_current_display * INVERSE_RAW_DATA_STEP_VALUE_current));
+    MDR_canopen_slave_node_->send_TPDO_2(0, 0, 0, 0);
+    MDR_canopen_slave_node_->send_TPDO_3(false); // TODO interlock_status
+    MDR_canopen_slave_node_->send_TPDO_4(0); // TODO rotor_position
+  }
 }
 
 void ROS2BridgeNode::motor_drive_fan_ros2_callback(agrosensebot_canopen_bridge_msgs::msg::MotorDrive::SharedPtr msg) {
-    if (FAN_canopen_slave_node_ != nullptr) {
-        FAN_canopen_slave_node_->send_TPDO_1(
-                (int16_t)(msg->controller_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
-                (int16_t)(msg->motor_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
-                msg->motor_rpm,
-                (int16_t)(msg->battery_current_display * INVERSE_RAW_DATA_STEP_VALUE_current));
-        FAN_canopen_slave_node_->send_TPDO_2(0, 0, 0, 0);
-        FAN_canopen_slave_node_->send_TPDO_3(false); // TODO interlock_status
-        FAN_canopen_slave_node_->send_TPDO_4(0); // TODO rotor_position
-    }
+  if (FAN_canopen_slave_node_ != nullptr) {
+    FAN_canopen_slave_node_->send_TPDO_1(
+            (int16_t) (msg->controller_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
+            (int16_t) (msg->motor_temperature * INVERSE_RAW_DATA_STEP_VALUE_temperature),
+            msg->motor_rpm,
+            (int16_t) (msg->battery_current_display * INVERSE_RAW_DATA_STEP_VALUE_current));
+    FAN_canopen_slave_node_->send_TPDO_2(0, 0, 0, 0);
+    FAN_canopen_slave_node_->send_TPDO_3(false); // TODO interlock_status
+    FAN_canopen_slave_node_->send_TPDO_4(0); // TODO rotor_position
+  }
 }
 
-void ROS2BridgeNode::gcu_is_alive_timer_ros2_callback(){
-    rclcpp::Time now = this->get_clock()->now();
+void ROS2BridgeNode::gcu_is_alive_timer_ros2_callback() {
+  rclcpp::Time now = this->get_clock()->now();
 
-    if(now - last_GCU_message_time_ > rclcpp::Duration(gcu_is_alive_timeout_)){
-        RCLCPP_ERROR(this->get_logger(), "GCU COMM TIMEOUT");
-        return;
-    }
+  if (now - last_GCU_message_time_ > rclcpp::Duration(gcu_is_alive_timeout_)) {
+    RCLCPP_ERROR(this->get_logger(), "GCU COMM TIMEOUT");
+    return;
+  }
 
-    if(now - last_GCU_alive_bit_change_time_ > rclcpp::Duration(gcu_is_alive_timeout_)){
-        RCLCPP_ERROR(this->get_logger(), "GCU ALIVE BIT CHANGE TIMEOUT");
-    }
+  if (now - last_GCU_alive_bit_change_time_ > rclcpp::Duration(gcu_is_alive_timeout_)) {
+    RCLCPP_ERROR(this->get_logger(), "GCU ALIVE BIT CHANGE TIMEOUT");
+  }
 }
 
 void ROS2BridgeNode::gcu_alive_canopen_callback(bool GCU_is_alive_bit, bool GCU_is_ready_bit, bool pump_cmd_bit) {
-    RCLCPP_INFO(this->get_logger(),
-                "gcu_alive_canopen_callback GCU_is_alive_bit: %i, GCU_is_ready_bit: %i, pump_cmd_bit: %i",
-                GCU_is_alive_bit, GCU_is_ready_bit, pump_cmd_bit);
+  RCLCPP_INFO(this->get_logger(),
+              "gcu_alive_canopen_callback GCU_is_alive_bit: %i, GCU_is_ready_bit: %i, pump_cmd_bit: %i",
+              GCU_is_alive_bit, GCU_is_ready_bit, pump_cmd_bit);
 
-    rclcpp::Time now = this->get_clock()->now();
-    last_GCU_message_time_ = now;
-    if (last_GCU_alive_bit_ != GCU_is_alive_bit) last_GCU_alive_bit_change_time_ = now;
-    last_GCU_alive_bit_ = GCU_is_alive_bit;
+  rclcpp::Time now = this->get_clock()->now();
+  last_GCU_message_time_ = now;
+  if (last_GCU_alive_bit_ != GCU_is_alive_bit) last_GCU_alive_bit_change_time_ = now;
+  last_GCU_alive_bit_ = GCU_is_alive_bit;
 //  TODO pub msg
 }
 
-void ROS2BridgeNode::speed_ref_canopen_callback(int16_t right_speed_ref, int16_t left_speed_ref, int16_t fan_speed_ref) {
-    RCLCPP_INFO(this->get_logger(),
-                "speed_ref_canopen_callback right_speed_ref: %i, left_speed_ref: %i, fan_speed_ref: %i",
-                right_speed_ref, left_speed_ref, fan_speed_ref);
+void
+ROS2BridgeNode::speed_ref_canopen_callback(int16_t right_speed_ref, int16_t left_speed_ref, int16_t fan_speed_ref) {
+  RCLCPP_INFO(this->get_logger(),
+              "speed_ref_canopen_callback right_speed_ref: %i, left_speed_ref: %i, fan_speed_ref: %i",
+              right_speed_ref, left_speed_ref, fan_speed_ref);
 
-    agrosensebot_canopen_bridge_msgs::msg::SpeedRef msg;
-    msg.stamp = this->get_clock()->now();
-    msg.right_speed_ref = right_speed_ref;
-    msg.left_speed_ref = left_speed_ref;
-    speed_ref_pub_->publish(msg);
+  agrosensebot_canopen_bridge_msgs::msg::SpeedRef msg;
+  msg.stamp = this->get_clock()->now();
+  msg.right_speed_ref = right_speed_ref;
+  msg.left_speed_ref = left_speed_ref;
+  speed_ref_pub_->publish(msg);
 }
