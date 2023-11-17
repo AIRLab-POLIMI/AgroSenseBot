be5149a22d5056dc69a78d014cf0ba83dce1caff
Enri2077 <enry2077@gmail.com>
2023-11-17 12:16:56 +0100

    only using ekf for map-odom tf

--- asb_sim/launch/dual_ekf_navsat.launch.py
+++ asb_sim/launch/dual_ekf_navsat.launch.py
@@ -32,14 +32,14 @@ def generate_launch_description():
             launch.actions.DeclareLaunchArgument(
                 "output_location", default_value="~/dual_ekf_navsat_example_debug.txt"
             ),
-            launch_ros.actions.Node(
-                package="robot_localization",
-                executable="ekf_node",
-                name="ekf_filter_node_odom",
-                output="screen",
-                parameters=[rl_params_file, {"use_sim_time": True}],
-                remappings=[("odometry/filtered", "odometry/local")],
-            ),
+            # launch_ros.actions.Node(
+            #     package="robot_localization",
+            #     executable="ekf_node",
+            #     name="ekf_filter_node_odom",
+            #     output="screen",
+            #     parameters=[rl_params_file, {"use_sim_time": True}],
+            #     remappings=[("odometry/filtered", "odometry/local")],
+            # ),
             launch_ros.actions.Node(
                 package="robot_localization",
                 executable="ekf_node",
