e615ff6e771ba5bc402aaa9119f5c1f7d680c7c7
Enri2077 <enry2077@gmail.com>
2023-11-21 12:24:43 +0100

    added options to use gazebo or webots in nav launch

--- asb_nav/launch/gnss_waypoint_follower.launch.py
+++ asb_nav/launch/gnss_waypoint_follower.launch.py
@@ -33,40 +33,50 @@ def generate_launch_description():
     nav2_params = os.path.join(package_share_dir, "config", "nav2_no_map_params.yaml")
     configured_params = RewrittenYaml(source_file=nav2_params, root_key="", param_rewrites={}, convert_types=True)
 
-    use_rviz = LaunchConfiguration('rviz')
-    use_mapviz = LaunchConfiguration('mapviz')
+    use_gazebo_launch_configuration = LaunchConfiguration('use_gazebo')
+    use_gazebo_launch_argument = DeclareLaunchArgument(
+        'use_gazebo',
+        default_value='False',
+        description='Whether to use Gazebo as simulator'
+    )
 
-    declare_use_rviz_cmd = DeclareLaunchArgument(
+    use_webots_launch_configuration = LaunchConfiguration('use_webots')
+    use_webots_launch_argument = DeclareLaunchArgument(
+        'use_webots',
+        default_value='True',
+        description='Whether to use Webots as simulator'
+    )
+
+    rviz_launch_configuration = LaunchConfiguration('rviz')
+    rviz_launch_argument = DeclareLaunchArgument(
         'rviz',
         default_value='False',
         description='Whether to start RViz'
     )
 
-    declare_use_mapviz_cmd = DeclareLaunchArgument(
+    mapviz_launch_configuration = LaunchConfiguration('mapviz')
+    mapviz_launch_argument = DeclareLaunchArgument(
         'mapviz',
         default_value='False',
         description='Whether to start MapViz'
     )
 
     gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(asb_gazebo_share_dir, "launch", "sim.launch.py"))
+        PythonLaunchDescriptionSource(os.path.join(asb_gazebo_share_dir, "launch", "sim.launch.py")),
+        condition=IfCondition(use_gazebo_launch_configuration)
     )
 
     webots_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(asb_webots_share_dir, "launch", "sim.launch.py"))
+        PythonLaunchDescriptionSource(os.path.join(asb_webots_share_dir, "launch", "sim.launch.py")),
+        condition=IfCondition(use_webots_launch_configuration)
     )
 
     robot_localization_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_share_dir, "launch", "dual_ekf_navsat.launch.py"))
+        PythonLaunchDescriptionSource(os.path.join(package_share_dir, "launch", "dual_ekf_navsat.launch.py"))
     )
 
     navigation2_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(bringup_share_dir, "launch", "navigation_launch.py")
-        ),
+        PythonLaunchDescriptionSource(os.path.join(bringup_share_dir, "launch", "navigation_launch.py")),
         launch_arguments={
             "use_sim_time": "True",
             "params_file": configured_params,
@@ -75,34 +85,36 @@ def generate_launch_description():
     )
 
     rviz_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_share_dir, "launch", "rviz.launch.py")),
-        condition=IfCondition(use_rviz)
+        PythonLaunchDescriptionSource(os.path.join(package_share_dir, "launch", "rviz.launch.py")),
+        condition=IfCondition(rviz_launch_configuration)
     )
 
     mapviz_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_share_dir, "launch", "mapviz.launch.py")),
-        condition=IfCondition(use_mapviz)
+        PythonLaunchDescriptionSource(os.path.join(package_share_dir, "launch", "mapviz.launch.py")),
+        condition=IfCondition(mapviz_launch_configuration)
     )
 
     # Create the launch description and populate
     ld = LaunchDescription()
 
-    # simulator launch
-    # ld.add_action(webots_launch)
-    ld.add_action(gazebo_launch)
+    # launch arguments
+    ld.add_action(use_gazebo_launch_argument)
+    ld.add_action(use_webots_launch_argument)
+    ld.add_action(rviz_launch_argument)
+    ld.add_action(mapviz_launch_argument)
 
-    # robot localization launch
+    # viz
+    ld.add_action(rviz_cmd)
+    ld.add_action(mapviz_cmd)
+
+    # sim
+    ld.add_action(gazebo_launch)
+    ld.add_action(webots_launch)
+
+    # localization
     ld.add_action(robot_localization_cmd)
 
-    # navigation2 launch
+    # navigation
     ld.add_action(navigation2_cmd)
-
-    # viz launch
-    ld.add_action(declare_use_rviz_cmd)
-    ld.add_action(rviz_cmd)
-    ld.add_action(declare_use_mapviz_cmd)
-    ld.add_action(mapviz_cmd)
 
     return ld
