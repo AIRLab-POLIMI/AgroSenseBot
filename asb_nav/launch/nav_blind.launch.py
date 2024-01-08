ad2806eb4eafdd2eb4c5dd8dc56bae1689e221dc
Enri2077 <enry2077@gmail.com>
2024-01-08 13:25:52 +0100

    using rpp

--- asb_nav/launch/nav_blind.launch.py
+++ asb_nav/launch/nav_blind.launch.py
@@ -59,8 +59,8 @@ def generate_launch_description():
         launch_arguments={
             "use_sim_time": use_sim_time_launch_configuration,
             "params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_params.yaml"),
-            "controller_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_controller_params_mppi.yaml"),
-            # "controller_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_controller_params_rpp.yaml"),
+            # "controller_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_controller_params_mppi.yaml"),
+            "controller_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_controller_params_rpp.yaml"),
             # "controller_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_controller_params_dwb.yaml"),
             "planner_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_planner_params_smac_hybrid.yaml"),
             # "planner_params_file": os.path.join(pkg("asb_nav"), "config", "nav_blind", "nav2_params", "nav2_planner_params_navfn.yaml"),
