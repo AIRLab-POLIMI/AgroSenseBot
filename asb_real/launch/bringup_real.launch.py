52168103aba33122e6f13930d271d3da510dad13
Enri2077 <enry2077@gmail.com>
2024-06-10 18:00:52 +0200

    set up front right OS0 lidar

--- asb_real/launch/bringup_real.launch.py
+++ asb_real/launch/bringup_real.launch.py
@@ -17,15 +17,18 @@ def generate_launch_description():
     )
 
     microstrain_3dm_gq7_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg("asb_real"), "launch", "gq7_launch.py")),
+        launch_description_source=PythonLaunchDescriptionSource(os.path.join(pkg("asb_real"), "launch", "gq7_launch.py")),
     )
 
-    # TODO lidar
-    fake_scan_node = Node(
-        package="asb_sim",
-        executable="fake_scan_pub.py",
-        name="fake_scan_publisher",
-        output="screen",
+    include_front_os0_launch = IncludeLaunchDescription(
+        # launch_description_source=PythonLaunchDescriptionSource(os.path.join(pkg("asb_real"), "launch", "ouster_os0_driver.launch.py")),
+        launch_description_source=PythonLaunchDescriptionSource(os.path.join(pkg("ouster_ros"), "launch", "driver.launch.py")),
+        launch_arguments={
+            "params_file": os.path.join(pkg("asb_real"), "config", "os0.yaml"),
+            # "os_driver_name": "scan_front_right_multilayer_driver",
+            "ouster_ns": "scan_front_right_multilayer",
+            "viz": "False",
+        }.items(),
     )
 
     test_heartbeat_publisher_node = Node(
@@ -38,8 +41,8 @@ def generate_launch_description():
     ld = launch.LaunchDescription()
 
     ld.add_action(include_control_launch)
-    # ld.add_action(microstrain_3dm_gq7_launch)
-    ld.add_action(fake_scan_node)
+    # ld.add_action(microstrain_3dm_gq7_launch)  # Launch separately to avoid resetting RTK fix when restarting nodes
+    ld.add_action(include_front_os0_launch)
     ld.add_action(test_heartbeat_publisher_node)
 
     return ld
