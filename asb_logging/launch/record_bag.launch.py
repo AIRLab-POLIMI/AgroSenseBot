1f2c005ca5402643bce15d6d7ba07c1afde87148
Enri2077 <enry2077@gmail.com>
2024-07-25 12:34:16 +0200

    record shorter bag segments to avoid sigkill while still compressing

--- asb_logging/launch/record_bag.launch.py
+++ asb_logging/launch/record_bag.launch.py
@@ -9,7 +9,7 @@ import launch.events
 def generate_launch_description():
 
     record_node = launch.actions.ExecuteProcess(
-        cmd="ros2 bag record -a --compression-mode file --compression-format zstd --max-bag-duration 600".split(),
+        cmd="ros2 bag record -a --compression-mode file --compression-format zstd --max-bag-duration 60".split(),
         cwd=os.path.expanduser("~/asb_logs/"),
         output='screen'
     )
