e247d43e5849d2bf75dfcb19f5e26e7431a2dfd7
Enri2077 <enry2077@gmail.com>
2024-07-29 18:59:34 +0200

    improved PID by adding forward component instead of proportional term

--- asb_track_controller/src/diff_drive_controller.cpp
+++ asb_track_controller/src/diff_drive_controller.cpp
@@ -326,18 +326,14 @@ controller_interface::return_type DiffDriveController::update(const rclcpp::Time
       angular_command_pid_.setGains(pp.p, pp.i, pp.d, pp.i_bound, -pp.i_bound, true);
     }
 
-    if(
-        (angular_vel_reference == 0.0) ||
-        (angular_vel_reference > 0 && last_imu_angular_velocity < 0) ||
-        (angular_vel_reference < 0 && last_imu_angular_velocity > 0)
-        )
+    if(angular_vel_reference == 0.0)
     {
       angular_command = 0.0;
       angular_command_pid_.reset();
     }
     else
     {
-      angular_command = angular_command_pid_.computeCommand(angular_vel_error, (uint64_t)period.nanoseconds());
+      angular_command = angular_vel_setpoint + angular_command_pid_.computeCommand(angular_vel_error, (uint64_t)period.nanoseconds());
     }
 
     if (params_.publish_pid_state)
