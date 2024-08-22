b97bfa7091d91ab159b74a6942c02bef8a08e0db
Enri2077 <enry2077@gmail.com>
2024-08-22 22:07:19 +0200

    added delay to control mode script

--- asb_sim/scripts/set_control_mode_manual_to_auto.py
+++ asb_sim/scripts/set_control_mode_manual_to_auto.py
@@ -2,6 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.time import Duration
 from asb_msgs.srv import SetControlMode, SetControlMode_Request
 
 
@@ -11,6 +12,7 @@ class ControlModePublisher(Node):
         super().__init__('control_mode_publisher')
 
         self.set_control_mode_service = self.create_client(SetControlMode, '/system_test/set_control_mode')
+        self.delay_sec = 1.0
 
         while not self.set_control_mode_service.wait_for_service(timeout_sec=1.0):
             self.get_logger().info(f"waiting {self.set_control_mode_service.srv_name} service")
@@ -20,6 +22,10 @@ class ControlModePublisher(Node):
 
     def set_manual_mode_response(self, response_future):
         self.get_logger().info(f"set manual mode response: {response_future.result().result}")
+
+        self.create_timer(self.delay_sec, self.delay_timer_callback)
+
+    def delay_timer_callback(self):
         set_auto_mode_future = self.set_control_mode_service.call_async(SetControlMode_Request(control_mode=SetControlMode_Request.CONTROL_MODE_GCU))  # set control mode to AUTO
         set_auto_mode_future.add_done_callback(self.set_auto_mode_response)
 
